43a72b7aa0f9136bac13305e4ccef14dfa1895e7
CristinaR08 <cris.04082009@hotmail.com>
2024-11-23 00:52:28 +0000

    Calculadora funcional - sin decimal

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,5 +1,5 @@
-var num1 = 0;
-var num2 = 0;
+var num1 = "";
+var num2 = "";
 var operacion = null;
 
 function setearValor(valor) {
@@ -13,47 +13,46 @@ function setearValorConcat(valor) {
     document.getElementById('idDisplay').innerText = valorActual + valor;
 }
 
-function recibirValor(valor){
+function recibirValor(valor) {
     console.log(valor);
     if (operacion === null) {
-        num1 = num1 *10 + valor;
+        num1 += valor;
         setearValor(num1);
     } else {
-        num2 = valor;
-        setearValorConcat(num2);
+        num2 += valor;
+        setearValorConcat(valor);
     }
 }
 
-function evaluarOperacion(op){
+function evaluarOperacion(op) {
     operacion = op;
     setearValor(op)
 }
 
 function igual() {
     let valorFinal = 0;
+    const numero1 = parseFloat(num1);
+    const numero2 = parseFloat(num2);
+
     if (operacion === '+') {
-        valorFinal = num1 + num2;
+        valorFinal = numero1 + numero2;
     } else if (operacion === '-') {
-        valorFinal = num1 - num2;
+        valorFinal = numero1 - numero2;
     } else if (operacion === 'X') {
-        valorFinal = num1 * num2;
+        valorFinal = numero1 * numero2;
     } else if (operacion === '/') {
-        if (num2 !== 0) {
-            valorFinal = num1 / num2;
-        } else {
-            valorFinal = "Math ERROR";
-        }
+        valorFinal = numero2 !== 0 ? numero1 / numero2 : "Math ERROR";
     }
 
     setearValor(valorFinal);
-    num1 = valorFinal === "number" ? valorFinal : 0;
-    num2 = 0;
+    num1 = typeof valorFinal === "number" ? valorFinal.toString() : "";
+    num2 = "";
     operacion = null;
 }
 
-function borrar(){
-    num1 = 0;
-    num2 = 0;
+function borrar() {
+    num1 = "";
+    num2 = "";
     operacion = null;
     setearValor(0);
 }
